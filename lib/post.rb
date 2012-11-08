8a5c83633d9a8906f317fdfcfe885faa73ffb2b0
ywjno <ywjno.dev@gmail.com>
2012-11-08 17:19:47 +0800

    tab convert to 2 spaces

--- lib/post.rb
+++ lib/post.rb
@@ -1,127 +1,129 @@
+# encoding: utf-8
+
 require 'rdiscount'
 require 'redcloth'
 
 class Post < Sequel::Model
-	include Rack::Utils
-	alias_method :h, :escape_html
+  include Rack::Utils
+  alias_method :h, :escape_html
 
-	Sequel.extension :pagination
-	plugin :schema
+  Sequel.extension :pagination
+  plugin :schema
 
-	unless table_exists?
-		set_schema do
-			primary_key :id
-			text :title, :null=>false
-			text :content, :null=>false
-			text :slug, :null=>false
-			text :tags, :null=>false
-			timestamp :created_at, :null=>false
-			Integer :delete_status, :null=>false, :default=> 1
-			text :format, :null=>false, :default=> "txt"
-		end
-		create_table
-	end
+  unless table_exists?
+    set_schema do
+      primary_key :id
+      text :title, :null=>false
+      text :content, :null=>false
+      text :slug, :null=>false
+      text :tags, :null=>false
+      timestamp :created_at, :null=>false
+      Integer :delete_status, :null=>false, :default=> 1
+      text :format, :null=>false, :default=> "txt"
+    end
+    create_table
+  end
 
-	def url
-		"/#{created_at.strftime('%Y/%m/%d')}/#{slug}/"
-	end
+  def url
+    "/#{created_at.strftime('%Y/%m/%d')}/#{slug}/"
+  end
 
-	def full_url
-		Blog.url_base.gsub(/\/$/, '') + url
-	end
+  def full_url
+    Blog.url_base.gsub(/\/$/, '') + url
+  end
 
-	def content_html
-		to_html(content.to_s, format)
-	end
+  def content_html
+    to_html(content.to_s, format)
+  end
 
-	def summary
-		@summary ||= content.match(/(.{200}.*?\n)/m)
-		@summary || content
-	end
+  def summary
+    @summary ||= content.match(/(.{200}.*?\n)/m)
+    @summary || content
+  end
 
-	def summary_html
-		to_html(summary.to_s, format)
-	end
+  def summary_html
+    to_html(summary.to_s, format)
+  end
 
-	def more?
-		@more ||= content.match(/.{200}.*?\n(.*)/m)
-		@more
-	end
+  def more?
+    @more ||= content.match(/.{200}.*?\n(.*)/m)
+    @more
+  end
 
-	def linked_tags
-		tags.split.inject([]) do |accum, tag|
-			accum << "<a href=\"/tags/#{tag}\">#{tag}</a>"
-		end.join(" ")
-	end
+  def linked_tags
+    tags.split.inject([]) do |accum, tag|
+      accum << "<a href=\"/tags/#{tag}\">#{tag}</a>"
+    end.join(" ")
+  end
 
-	def self.make_slug(title)
-		slug = URI.escape(title.downcase.gsub(/[ _]/, '-')).gsub(/[^a-zA-Z0-9%\-]/, '').squeeze('-')
-		unless Post.filter(:slug => slug).first
-			slug
-		else
-			count = Post.filter(:slug.like("#{slug}-%")).count + 1
-			"#{slug}-#{(count + 1)}"
-		end
-	end
+  def self.make_slug(title)
+    slug = URI.escape(title.downcase.gsub(/[ _]/, '-')).gsub(/[^a-zA-Z0-9%\-]/, '').squeeze('-')
+    unless Post.filter(:slug => slug).first
+      slug
+    else
+      count = Post.filter(:slug.like("#{slug}-%")).count + 1
+      "#{slug}-#{(count + 1)}"
+    end
+  end
 
-	def delete?
-		delete_status == 0
-	end
+  def delete?
+    delete_status == 0
+  end
 
-	def self.dates(admin)
-		dates = {}
-		posts = nil
-		if admin
-			posts = Post.reverse_order(:created_at)
-		else
-			posts = Post.filter(:delete_status => 1).reverse_order(:created_at)
-		end
-		posts.each do |post|
-			dates[post.created_at.strftime("%Y/%m")] = post.created_at.strftime("%Y-%m") unless dates[post.created_at.strftime("%Y/%m")]
-		end
-		dates
-	end
+  def self.dates(admin)
+    dates = {}
+    posts = nil
+    if admin
+      posts = Post.reverse_order(:created_at)
+    else
+      posts = Post.filter(:delete_status => 1).reverse_order(:created_at)
+    end
+    posts.each do |post|
+      dates[post.created_at.strftime("%Y/%m")] = post.created_at.strftime("%Y-%m") unless dates[post.created_at.strftime("%Y/%m")]
+    end
+    dates
+  end
 
-	def show_create_at
-		created_at.strftime("%Y-%m-%d %H:%M:%S")
-	end
+  def show_create_at
+    created_at.strftime("%Y-%m-%d %H:%M:%S")
+  end
 
-	########
+  ########
 
-	def to_html(content, format = 'txt')
-		return case format
-			when 'markdown'
-				RDiscount.new(content).to_html
-			when 'textile'
-				RedCloth.new(content).to_html
-			else
-				split_content(content)
-			end
-	end
+  def to_html(content, format = 'txt')
+    return case format
+      when 'markdown'
+        RDiscount.new(content).to_html
+      when 'textile'
+        RedCloth.new(content).to_html
+      else
+        split_content(content)
+      end
+  end
 
-	def split_content(string)
-		show_html = ""
-		p_content = []
-		lines = string.split("\n")
-		lines.each_with_index do |line, index|
-			new_line = h(line.strip)
-			if index != lines.length - 1
-				unless new_line.empty?
-					p_content << new_line
-				else
-					show_html += "<p>#{p_content.join('<br />')}</p>"
-					p_content = []
-				end
-			else
-				if p_content.length != 0
-					p_content << new_line
-					show_html += "<p>#{p_content.join("<br />")}</p>"
-				else
-					show_html += "<p>#{new_line}</p>"
-				end
-			end
-		end
-		show_html
-	end
+  def split_content(string)
+    show_html = ""
+    p_content = []
+    lines = string.split("\n")
+    lines.each_with_index do |line, index|
+      new_line = h(line.strip)
+      if index != lines.length - 1
+        unless new_line.empty?
+          p_content << new_line
+        else
+          show_html += "<p>#{p_content.join('<br />')}</p>"
+          p_content = []
+        end
+      else
+        if p_content.length != 0
+          p_content << new_line
+          show_html += "<p>#{p_content.join("<br />")}</p>"
+        else
+          show_html += "<p>#{new_line}</p>"
+        end
+      end
+    end
+    show_html
+  end
 
 end
--- main.rb
+++ main.rb
@@ -1,199 +1,201 @@
+# encoding: utf-8
+
 require 'rubygems'
 require 'sinatra'
 require 'digest/sha1'
 require 'sequel'
 
 configure do
-	DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://blog.db')
+  DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://blog.db')
 
-	require 'ostruct'
-	Blog = OpenStruct.new(
-		:title => 'a scanty blog',
-		:subtitle => 'Scanty, a really small blog',
-		:author => 'John Doe',
-		:url_base => 'http://localhost:4567/',
-		:admin_password => Digest::SHA1.hexdigest('changeme'),
-		:admin_cookie_key => 'scanty_admin',
-		:admin_cookie_value => Digest::SHA1.hexdigest('51d6d976913ace58'),
-		:disqus_shortname => nil,
-		:page_size => 10,
-		:timezone => '+08:00'
-	)
+  require 'ostruct'
+  Blog = OpenStruct.new(
+    :title => 'a scanty blog',
+    :subtitle => 'Scanty, a really small blog',
+    :author => 'John Doe',
+    :url_base => 'http://localhost:4567/',
+    :admin_password => Digest::SHA1.hexdigest('changeme'),
+    :admin_cookie_key => 'scanty_admin',
+    :admin_cookie_value => Digest::SHA1.hexdigest('51d6d976913ace58'),
+    :disqus_shortname => nil,
+    :page_size => 10,
+    :timezone => '+08:00'
+  )
 end
 
 error do
-	e = request.env['sinatra.error']
-	puts e.to_s
-	puts e.backtrace.join("\n")
-	"Application error"
+  e = request.env['sinatra.error']
+  puts e.to_s
+  puts e.backtrace.join("\n")
+  "Application error"
 end
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
 require 'post'
 
 helpers do
-	def admin?
-		request.cookies[Blog.admin_cookie_key] == Blog.admin_cookie_value
-	end
+  def admin?
+    request.cookies[Blog.admin_cookie_key] == Blog.admin_cookie_value
+  end
 
-	def auth
-		halt [ 401, 'Not authorized' ] unless admin?
-	end
+  def auth
+    halt [ 401, 'Not authorized' ] unless admin?
+  end
 
-	def paginate(post, options={})
-		return "" if post.page_count == 1
-		html = '<div class="paginate">'
-		url = ""
-		url = "/tags/#{options[:tag]}" if options[:tag]
-		if post.prev_page
-			html += "<p class=\"pull-left\"><a href=\"#{url}/page/#{post.prev_page}\">&larr;&nbsp;Previous</a></p>"
-		end
-		if post.next_page
-			html += "<p class=\"pull-right\"><a href=\"#{url}/page/#{post.next_page}\">Next&nbsp;&rarr;</a></p>"
-		end
-		html += "</div>"
-	end
+  def paginate(post, options={})
+    return "" if post.page_count == 1
+    html = '<div class="paginate">'
+    url = ""
+    url = "/tags/#{options[:tag]}" if options[:tag]
+    if post.prev_page
+      html += "<p class=\"pull-left\"><a href=\"#{url}/page/#{post.prev_page}\">&larr;&nbsp;Previous</a></p>"
+    end
+    if post.next_page
+      html += "<p class=\"pull-right\"><a href=\"#{url}/page/#{post.next_page}\">Next&nbsp;&rarr;</a></p>"
+    end
+    html += "</div>"
+  end
 end
 
 ### Public
 
 get '/' do
-	posts = Post.filter(:delete_status => 1).reverse_order(:created_at).paginate(1, Blog.page_size)
-	erb :index, :locals => { :posts => posts, :dates => Post.dates(admin?) }, :layout => :sidebar_layout
+  posts = Post.filter(:delete_status => 1).reverse_order(:created_at).paginate(1, Blog.page_size)
+  erb :index, :locals => { :posts => posts, :dates => Post.dates(admin?) }, :layout => :sidebar_layout
 end
 
 get %r{^/\d{4}/\d{2}/\d{2}/(?<slug>[a-zA-Z0-9%\-]+)/?$} do
-	posts = nil
-	if admin?
-		post = Post.filter(:slug => URI.escape(params[:slug])).first
-	else
-		post = Post.filter(:delete_status => 1).filter(:slug => URI.escape(params[:slug])).first
-	end
-	halt [ 404, "Page not found" ] unless post
-	erb :post, :locals => { :post => post }, :layout => :layout
+  posts = nil
+  if admin?
+    post = Post.filter(:slug => URI.escape(params[:slug])).first
+  else
+    post = Post.filter(:delete_status => 1).filter(:slug => URI.escape(params[:slug])).first
+  end
+  halt [ 404, "Page not found" ] unless post
+  erb :post, :locals => { :post => post }, :layout => :layout
 end
 
 get '/archive' do
-	posts = nil
-	if admin?
-		posts = Post.reverse_order(:created_at)
-	else
-		posts = Post.filter(:delete_status => 1).reverse_order(:created_at)
-	end
-	erb :archive, :locals => { :posts => posts }, :layout => :layout
+  posts = nil
+  if admin?
+    posts = Post.reverse_order(:created_at)
+  else
+    posts = Post.filter(:delete_status => 1).reverse_order(:created_at)
+  end
+  erb :archive, :locals => { :posts => posts }, :layout => :layout
 end
 
 get '/tags/:tag' do
-	tag = params[:tag]
-	posts = Post.filter(:delete_status => 1).filter(:tags.like("%#{tag}%")).reverse_order(:created_at).paginate(1, Blog.page_size)
-	erb :tagged, :locals => { :posts => posts, :tag => tag }, :layout => :layout
+  tag = params[:tag]
+  posts = Post.filter(:delete_status => 1).filter(:tags.like("%#{tag}%")).reverse_order(:created_at).paginate(1, Blog.page_size)
+  erb :tagged, :locals => { :posts => posts, :tag => tag }, :layout => :layout
 end
 
 get '/page/:page' do
-	posts = Post.filter(:delete_status => 1).reverse_order(:created_at).paginate(params[:page].to_i, Blog.page_size)
-	redirect '/' if posts.page_count < params[:page].to_i
-	erb :index, :locals => { :posts => posts, :dates => Post.dates(admin?) }, :layout => :sidebar_layout
+  posts = Post.filter(:delete_status => 1).reverse_order(:created_at).paginate(params[:page].to_i, Blog.page_size)
+  redirect '/' if posts.page_count < params[:page].to_i
+  erb :index, :locals => { :posts => posts, :dates => Post.dates(admin?) }, :layout => :sidebar_layout
 end
 
 get '/tags/:tag/page/:page' do
-	tag = params[:tag]
-	posts = Post.filter(:delete_status => 1).filter(:tags.like("%#{tag}%")).reverse_order(:created_at).paginate(params[:page].to_i, Blog.page_size)
-	redirect '/' if posts.page_count < params[:page].to_i
-	erb :tagged, :locals => { :posts => posts, :tag => tag }, :layout => :layout
+  tag = params[:tag]
+  posts = Post.filter(:delete_status => 1).filter(:tags.like("%#{tag}%")).reverse_order(:created_at).paginate(params[:page].to_i, Blog.page_size)
+  redirect '/' if posts.page_count < params[:page].to_i
+  erb :tagged, :locals => { :posts => posts, :tag => tag }, :layout => :layout
 end
 
 ["/rss", "/feed"].each do |path|
-	get path do
-		@posts = Post.filter(:delete_status => 1).reverse_order(:created_at).limit(20)
-		content_type 'application/atom+xml', :charset => 'utf-8'
-		builder :feed
-	end
+  get path do
+    @posts = Post.filter(:delete_status => 1).reverse_order(:created_at).limit(20)
+    content_type 'application/atom+xml', :charset => 'utf-8'
+    builder :feed
+  end
 end
 
 get %r{^/(?<year>\d{4})/(?<month>\d{2})/?$} do
-	all_posts = nil
-	if admin?
-		all_posts = Post.reverse_order(:created_at)
-	else
-		all_posts = Post.filter(:delete_status => 1).reverse_order(:created_at)
-	end
+  all_posts = nil
+  if admin?
+    all_posts = Post.reverse_order(:created_at)
+  else
+    all_posts = Post.filter(:delete_status => 1).reverse_order(:created_at)
+  end
 
-	posts = []
-	all_posts.each do |post|
-		posts << post if post.created_at.strftime("%Y") == params[:year] and post.created_at.strftime("%m") == params[:month]
-	end
+  posts = []
+  all_posts.each do |post|
+    posts << post if post.created_at.strftime("%Y") == params[:year] and post.created_at.strftime("%m") == params[:month]
+  end
 
-	erb :archive, :locals => { :posts => posts }, :layout => :layout
+  erb :archive, :locals => { :posts => posts }, :layout => :layout
 end
 
 ### Admin
 
 get '/auth' do
-	erb :auth, :locals => { :error => false }
+  erb :auth, :locals => { :error => false }
 end
 
 post '/auth' do
-	if Digest::SHA1.hexdigest(params[:password]) == Blog.admin_password
-		response.set_cookie(Blog.admin_cookie_key, Blog.admin_cookie_value)
-		redirect '/'
-	else
-		erb :auth, :locals => { :error => true }
-	end
+  if Digest::SHA1.hexdigest(params[:password]) == Blog.admin_password
+    response.set_cookie(Blog.admin_cookie_key, Blog.admin_cookie_value)
+    redirect '/'
+  else
+    erb :auth, :locals => { :error => true }
+  end
 end
 
 get '/logout' do
-	response.delete_cookie(Blog.admin_cookie_key)
-	redirect '/'
+  response.delete_cookie(Blog.admin_cookie_key)
+  redirect '/'
 end
 
 get '/posts/new' do
-	auth
-	erb :edit, :locals => { :post => Post.new, :url => '/posts' }
+  auth
+  erb :edit, :locals => { :post => Post.new, :url => '/posts' }
 end
 
 post '/posts' do
-	auth
-	post = nil
-	DB.transaction do
-		post = Post.new :title => params[:title],
-						:tags => params[:tags],
-						:content => params[:content],
-						:created_at => Time.now.utc.getlocal(Blog.timezone),
-						:slug => Post.make_slug(params[:title]),
-						:format => params[:format]
-		post.save
-	end
-	redirect post.url
+  auth
+  post = nil
+  DB.transaction do
+    post = Post.new :title => params[:title],
+            :tags => params[:tags],
+            :content => params[:content],
+            :created_at => Time.now.utc.getlocal(Blog.timezone),
+            :slug => Post.make_slug(params[:title]),
+            :format => params[:format]
+    post.save
+  end
+  redirect post.url
 end
 
 get %r{^/\d{4}/\d{2}/\d{2}/(?<slug>[a-zA-Z0-9%\-]+)/edit/?$} do
-	auth
-	post = Post.filter(:slug => URI.escape(params[:slug])).first
-	halt [ 404, "Page not found" ] unless post
-	erb :edit, :locals => { :post => post, :url => post.url }
+  auth
+  post = Post.filter(:slug => URI.escape(params[:slug])).first
+  halt [ 404, "Page not found" ] unless post
+  erb :edit, :locals => { :post => post, :url => post.url }
 end
 
 post %r{^/\d{4}/\d{2}/\d{2}/(?<slug>[a-zA-Z0-9%\-]+)/$} do
-	auth
-	delete_status = params[:delete_status] ? 0 : 1
-	post = nil
-	DB.transaction do
-		post = Post.filter(:slug => URI.escape(params[:slug])).first
-		halt [ 404, "Page not found" ] unless post
-		unless params[:delete_status]
-			post.title = params[:title]
-			post.tags = params[:tags]
-			post.content = params[:content]
-			post.slug = Post.make_slug(params[:title]) if params[:change_slug]
-			post.format = params[:format]
-		end
-		post.delete_status = delete_status
-		post.save
+  auth
+  delete_status = params[:delete_status] ? 0 : 1
+  post = nil
+  DB.transaction do
+    post = Post.filter(:slug => URI.escape(params[:slug])).first
+    halt [ 404, "Page not found" ] unless post
+    unless params[:delete_status]
+      post.title = params[:title]
+      post.tags = params[:tags]
+      post.content = params[:content]
+      post.slug = Post.make_slug(params[:title]) if params[:change_slug]
+      post.format = params[:format]
+    end
+    post.delete_status = delete_status
+    post.save
 
-		if params[:delete_status]
-			redirect '/'
-		else
-			redirect post.url
-		end
-	end
+    if params[:delete_status]
+      redirect '/'
+    else
+      redirect post.url
+    end
+  end
 end
